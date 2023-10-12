903530892ae80a93d5bfc6a7d065235b3ed3705d
Dorapower <thatisluo@outlook.com>
2023-10-12 12:03:10 +0800

    feat: add content

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Python-Project-Template
+
 Start point for a python package/app
+
+I didn't include JetBrain's config folder here since it will create a new one when opening the new project.
---
+++ example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+<Description>
+
+Author: Lokdora
+Date: 2023-10-12
+"""
+
+import sys
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())
